fc6144139a8b8f1422289ce461c8ddf8136189dc
21parshant <parshant.rao@samviddhi.com>
2024-01-10 12:10:05 +0530

    fixed nav bar

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,14 +1,48 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import "./Header.css"
 import { useNavigate } from "react-router-dom";
 export default function Header(props) {
     let navigate = useNavigate()
     const { pageOpen, setPageOpen,mobileView } = props
+    const [needMargin, setNeedMargin] = useState(false)
 
+    function debounce(func, delay) {
+        let timeoutId;
+        return function () {
+            const context = this;
+            const args = arguments;
+            clearTimeout(timeoutId);
+            timeoutId = setTimeout(() => {
+                func.apply(context, args);
+            }, delay);
+        };
+    }
+
+
+    const handleResize = debounce(() => {
+        if (window.innerWidth >= 425) {
+            setNeedMargin(true)
+        }
+        else setNeedMargin(false)
+    }, 200); // Adjust the delay as needed
+
+    useEffect(() => {
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, [handleResize]);
+
+    useEffect(()=>{
+        if (window.innerWidth > 425) {
+            console.log(window.innerWidth)
+            setNeedMargin(true)
+        }
+    },[])
     return (
         <>
-            <div className={mobileView ? "header-content-mobile-view" : "header-content"}>
-                <div className="me-5" style={{ display: 'flex', flexDirection: 'column', cursor: 'pointer' }} onClick={() => {
+            <div className={mobileView ? "header-content-mobile-view" : "header-content"} style={needMargin?{display:'flex',justifyContent:'center'}:{}}>
+                <div className={needMargin?"me-5":""} style={{ display: 'flex', flexDirection: 'column', cursor: 'pointer' }} onClick={() => {
                     setPageOpen("")
                 }}>
                     <span style={pageOpen === "" ? { color: '#434343' } : { color: '#589cf7' }} >
@@ -20,7 +54,7 @@ export default function Header(props) {
                     {pageOpen === "" ? <span className="dash" /> : ""}
                 </div>
 
-                <div className="me-5" style={{ display: 'flex', flexDirection: 'column', cursor: 'pointer' }} onClick={() => {
+                <div className={needMargin?"me-5":""} style={{ display: 'flex', flexDirection: 'column', cursor: 'pointer' }} onClick={() => {
                     setPageOpen("Papers")
                 }}>
                     <span style={pageOpen === "Papers" ? { color: '#434343' } : { color: '#589cf7' }} >Papers</span>
